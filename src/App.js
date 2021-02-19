bf9645a406909e278cf023b823264198e9a74ebf
Andrey Semenov <andss82@gmail.com>
2021-02-19 09:43:48 +0300

    add hide\show

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card/Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = {
+  title: 'Card',
+  text: 'lorem ipsum dolor sit amet, consectetur adipiscing',
 }
 
-export default App;
+function App() {
+  const [hide, setHide] = useState(false)
+
+  const onToggle = () => {
+    setHide(!hide)
+  }
+
+  const dataRest = { ...data, hide }
+
+  return (
+    <div className="App">
+      <Card {...dataRest} onToggle={onToggle} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 200px;
+  padding: 10px;
+  border: 2px solid #bebebe;
+  border-radius: 6px;
+`
+
+const Title = styled.h2`
+  margin: 0;
+  text-align: center;
+`
+
+const Text = styled.p`
+  font-size: 20px;
+  opacity: 1;
+  transition: opacity 0.2s linear;
+
+  &.collapse {
+    font-size: 0;
+    opacity: 0;
+    transition: font-size 0.25s linear;
+  }
+`
+
+const Button = styled.button`
+  cursor: pointer;
+  border-style: none;
+  border-bottom: 1px dashed grey;
+  background-color: inherit;
+`
+
+function Card(props) {
+  const onToggle = (event) => {
+    event.target.blur()
+    props.onToggle()
+  }
+
+  return (
+    <Container>
+      <Title>{props.title}</Title>
+      <Text className={props.hide ? 'collapse' : 'expand'}>{props.text}</Text>
+      <Button onClick={onToggle}>{props.hide ? 'Show ˅' : 'hide ˄'}</Button>
+    </Container>
+  )
+}
+
+Card.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  onToggle: PropTypes.func.isRequired,
+  hide: PropTypes.bool.isRequired,
+}
+
+export default Card
